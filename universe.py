65bc48726cd101692e9966ddd7f2f82f446d4ad4
tdragon6 <95691624+tdragon6@users.noreply.github.com>
2026-03-27 23:28:14 +0800

    Update user request management logic
    Refactor user request handling to track request status and manage session timeouts.

--- universe.py
+++ universe.py
@@ -8,8 +8,10 @@ import logging
 from logging.handlers import RotatingFileHandler
 from concurrent.futures import ThreadPoolExecutor
 
+
 # 根目录
 root_dir = os.path.dirname(os.path.abspath(__file__))
+
 
 # 日志配置
 logger = logging.getLogger(__name__)
@@ -73,6 +75,7 @@ hint_message_dict = {
     "cool_down": "上一个请求正在处理中，请一个一个来~"
 }
 
+
 # 消息类型描述
 msg_type_dict = {
     "image": "图片",
@@ -80,6 +83,7 @@ msg_type_dict = {
     "voice": "语音",
     "file": "文件"
 }
+
 
 # 文件类型映射消息类型
 file_type_to_media_type_dict = {
@@ -103,9 +107,14 @@ file_size_limit = {
 supported_msg_types = ["text", "voice", "image", "video", "file", "location"]
 
 
-# 用户请求限制
-# user_id + bot_type 为key，value为请求时间戳
-# 请求会话时添加key和value，会话结束时删除key和value，确保同一用户同一bot同一时间只能进行一个会话
-# 若存在key，判断时间戳小于当前时间15min内则拦截请求，否则更新value为当前时间戳，继续处理
-user_request_limit = {}
- 
+# 用户请求状态
+# user_id + bot_type 为key，value为字典，包含message、in_progress、update_time字段
+# message为用户请求消息，in_progress为是否正在处理中，update_time为最近一次消息时间戳
+# 单次消息请求时初始化或更新key和value，单次会话结束时删除key和value，确保同一用户同一bot同一时间只能进行一个会话
+# 单次消息请求后的10秒内若收到后续消息，则认为是一次会话请求，拼接message字段，以此类推处理；若某次消息请求10秒内无后续消息请求，设置in_progress为True，开始处理这次会话所有message
+# 若存在key，且单次会话在处理中，且最近一次消息时间戳小于当前时间15min内则拦截请求，否则允许请求
+user_request_status = {}
+
+
+# 被认为归属一次用户会话请求的时间间隔
+user_one_request_interval = 10
